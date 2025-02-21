cdaae570a70cff7c773068ac612100b6f7c6122b
klavrenuk <klavrenuk1993@gmail.com>
2025-02-21 06:40:02 +0300

    init

---
+++ src/types/auth.ts
@@ -0,0 +1,10 @@
+export interface Login {
+    login: string,
+    password: string
+}
+
+export interface Registration {
+    login: string,
+    password: string,
+    passwordRepeat: string
+}
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    content: [
+      "./src/**/*.{js,jsx,ts,tsx}", // Указываем, где искать классы Tailwind
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
